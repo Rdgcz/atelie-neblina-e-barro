823dcbcb651d59add22fcee4ffcfe42d78dc7e21
Rdgcz <rdgcz10@gmail.com>
2025-11-26 21:08:41 -0300

    minor updates

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ navLinks.forEach(link => {
 // ANIMAÇÕES ON-SCROLL
 // =====================================================================
 
-const reveals = document.querySelectorAll('.reveal, .reveal-left, .reveal-right, .reveal-up');
+const reveals = document.querySelectorAll('.reveal, .reveal-left, .reveal-right');
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -41,7 +41,7 @@ const observer = new IntersectionObserver((entries) => {
       observer.unobserve(entry.target);
     }
   });
-}, { threshold: 0.15 });
+}, { threshold: 0.2 });
 
 reveals.forEach(el => observer.observe(el));
 
