997595db7c6263060007d45a7eb5bffb78ad4e9d
Rdgcz <rdgcz10@gmail.com>
2025-11-27 20:09:57 -0300

    final adjustments

--- main.js
+++ main.js
@@ -38,10 +38,9 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
     }
   });
-}, { threshold: 0.2 });
+}, { threshold: 0.18 });
 
 reveals.forEach(el => observer.observe(el));
 
